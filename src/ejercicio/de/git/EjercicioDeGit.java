23bdc071d4edc8163340b892ebf83466c3b8598f
Eduardo García <eduardo.garcia.ordonez@outlook.com>
2024-07-11 11:58:00 -0600

    Repositorio

---
+++ src/ejercicio/de/git/EjercicioDeGit.java
@@ -0,0 +1,15 @@
+
+package ejercicio.de.git;
+
+
+public class EjercicioDeGit {
+
+    
+    public static void main(String[] args) {
+        
+        for (int i = 1; i < 11; i++) {
+            System.out.println("10X"+i+"="+i*10);
+        }
+    }
+    
+}
